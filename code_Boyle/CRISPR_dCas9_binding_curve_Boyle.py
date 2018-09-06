8a08647d5d7f431794c3f0279e6242e66479d2ee
Misha Klein <klein.misha@gmail.com>
2018-09-06 12:33:05 +0200

    for merge

--- code_Boyle/CRISPR_dCas9_binding_curve_Boyle.py
+++ code_Boyle/CRISPR_dCas9_binding_curve_Boyle.py
@@ -1,9 +1,9 @@
 import numpy as np
 from scipy import linalg
 import sys
-PATH_HPC05 = '/home/mklein1/Energy_Landscape_dCas9/'
-sys.path.append(PATH_HPC05)
-#sys.path.append('../code_general/')
+# PATH_HPC05 = '/home/mklein1/Energy_Landscape_dCas9/'
+# sys.path.append(PATH_HPC05)
+sys.path.append('../code_general/')
 from CRISPR_free_energy_landscape import unpack_parameters
 
 ''''
@@ -21,10 +21,6 @@ def calc_Chi_square(parameters,xdata,ydata,yerr,
                     weights=np.array([1.0,1.0,1.0]),
                     guide_length=20, model_id='general_energies'):
     '''
-
-    ********** ADJUSTED THIS FUNCTION AFTER BEHROUZ TESTED IT *******
-    **** SHOULD CHECK IF UNPACKING OF PARAMTERS WORKED GOOD ********
-
     *** To make the Simmulated Annealing general I added the 'yerr' as argument,
     but it is really not used in our case ******************
 
