9e74fe7509ea9f9d0efcd9bf6a487845685bee5f
beslami <behrooz.eslami@gmail.com>
2018-09-09 19:03:35 +0200

    Updated with a new model "init_limit_5EpsC_2EpsI"

--- code_general/CRISPR_free_energy_landscape.py
+++ code_general/CRISPR_free_energy_landscape.py
@@ -8,7 +8,8 @@ choose_model = ['general_energies',
                 'constant_eps_I',
                 'init_limit_lock_const_EpsI',
                 'init_limit_two_drops_fixed_BP',
-                'init_limit_two_drops']
+                'init_limit_two_drops',
+                'init_limit_5EpsC_2EpsI']
 
 
 
@@ -24,6 +25,8 @@ def unpack_parameters(parameters, model_id='general_energies',guide_length=20):
     '''
 
     epsilon = np.zeros(2 * guide_length + 1)
+    forward_rates = np.ones(guide_length + 2)
+
     if model_id == 'general_energies':
         # General position dependency + minimal amount of rates
         epsilon = parameters[:-2]
@@ -101,6 +104,7 @@ def unpack_parameters(parameters, model_id='general_energies',guide_length=20):
         forward_rates[-1] = 0.0
 
     if model_id == 'init_limit_two_drops':
+
         e_PAM = parameters[0]
         ec_1 = parameters[1]
         ec_first = parameters[2]
@@ -129,6 +133,39 @@ def unpack_parameters(parameters, model_id='general_energies',guide_length=20):
         forward_rates[0] = k_PAM
         forward_rates[1] = k_1
         forward_rates[-1] = 0.0
+
+    if model_id == 'init_limit_5EpsC_2EpsI':
+        e_PAM = parameters[0]
+        ec_1 = parameters[1]
+        ec_2 = parameters[2]
+        ec_3 = parameters[3]
+        ec_4 = parameters[4]
+        ec_5 = parameters[5]
+        eI_1 = parameters[6]
+        eI_2 = parameters[7]
+        bp2 = parameters[8]
+        bp3 = parameters[9]
+        bp4 = parameters[10]
+        bpI = parameters[11]
+        k_PAM = parameters[12]
+        E_barr = parameters[13]
+        k = parameters[14]
+        k_1 = k * np.exp(-E_barr)
+
+        epsilon[0] = e_PAM
+        epsilon[1] = ec_1
+        epsilon[2:bp2 + 1] = ec_2
+        epsilon[bp2+1:bp3 + 1] = ec_3
+        epsilon[bp3 + 1:bp4 + 1] = ec_4
+        epsilon[bp4 + 1:guide_length + 1] = ec_5
+        epsilon[guide_length + 1:guide_length + bpI + 1] = eI_1
+        epsilon[guide_length + bpI + 1:] = eI_2
+
+        forward_rates = np.ones(guide_length + 2) * k  # internal rates
+        forward_rates[0] = k_PAM
+        forward_rates[1] = k_1
+        forward_rates[-1] = 0.0
+
     return epsilon, forward_rates
 
 
@@ -168,13 +205,13 @@ def plot_mismatch_penalties(parameters, model_id):
     '''
     epsilon, fwrd_rates = unpack_parameters(parameters, model_id, guide_length=20)
     epsilon_I = epsilon[21:]
-    plt.bar([i + 1 for i in range(20)], epsilon_I)
+    plt.bar(range(1,21), epsilon_I)
 
     # window dressing:
     plt.xlabel('targeting progression', fontsize=15)
-    plt.ylabel(r'free-energy ($k_BT$)',fontsize=15)
-    plt.xticks([i for i in range(1,21)],
+    plt.ylabel(r'mismatch penalties ($k_BT$)',fontsize=15)
+    plt.xticks(np.arange(1,21)+0.5,
                [1, '', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20], rotation=0
                ,fontsize=15);
     plt.yticks(fontsize=15)
-    return
+    return epsilon_I
