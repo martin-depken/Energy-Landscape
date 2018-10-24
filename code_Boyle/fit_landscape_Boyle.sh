40b624097f70095c66e982cb1bf1b41b78b239ea
Misha Klein <klein.misha@gmail.com>
2018-10-24 13:24:57 +0200

    fits 19_10_2018

--- code_Boyle/fit_landscape_Boyle.sh
+++ code_Boyle/fit_landscape_Boyle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
 
-parameters=$(sed -n -e "${PBS_ARRAYID}p" /home/mklein1/Energy_Landscape_dCas9/13_9_2018/jobs_13_9_2018_V_W_X.txt)
+parameters=$(sed -n -e "${PBS_ARRAYID}p" /home/mklein1/Energy_Landscape_dCas9/19_10_2018/jobs_19_10_2018_21_23.txt)
 parameterArray=($parameters)
 
 ReplicaID=${parameterArray[0]}
@@ -11,3 +11,5 @@ OutputResults=${parameterArray[3]}
 OutputInitMonitor=${parameterArray[4]}
 UseHPC05=${parameterArray[5]}
 python2  /home/mklein1/Energy_Landscape_dCas9/fit_landscape_Boyle.py $ReplicaID $ModelID $OutputMonitor $OutputResults $OutputInitMonitor $UseHPC05
+
+
--- code_general/read_model_ID.py
+++ code_general/read_model_ID.py
@@ -39,6 +39,12 @@ def unpack_parameters(parameters, model_id='general_energies',guide_length=20):
         forward_rates[0] = parameters[-1]  # from solution to PAM
         forward_rates[-1] = 0.0  # dCas9 does not cleave
 
+    if model_id == 'general_energies_v2':
+        # General position dependency + minimal amount of rates
+        epsilon = parameters[:-2]
+        forward_rates = np.ones(guide_length + 2) * 10**parameters[-2] #internal rates
+        forward_rates[0] = 10**parameters[-1]  # from solution to PAM
+        forward_rates[-1] = 0.0  # dCas9 does not cleave
 
     if model_id == 'init_limit_general_energies_v0':
         # General position dependency
