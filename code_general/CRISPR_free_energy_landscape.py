044c24f33771fe6e3a80a18be3af0431ac537c2c
Misha Klein <klein.misha@gmail.com>
2018-10-24 13:41:26 +0200

    made plotting optional

--- code_general/CRISPR_free_energy_landscape.py
+++ code_general/CRISPR_free_energy_landscape.py
@@ -7,7 +7,7 @@ reload(read_model_ID);
 
 
 
-def plot_free_energy_landscape(parameters,model_id):
+def plot_free_energy_landscape(parameters,model_id,show_plot=True):
     '''
     Plot the approximate the free-energy landscape for the on-target
 
@@ -32,16 +32,18 @@ def plot_free_energy_landscape(parameters,model_id):
     for eps in epsilon_C:
         landscape.append(landscape[-1] + eps)
     landscape = np.array(landscape)
-    plt.figure()
-    plt.plot(range(-1,21),landscape, marker='s');
-    plt.xlabel('targeting progression', fontsize=15)
-    plt.ylabel(r'(approx.) free-energy ($k_BT$)',fontsize=15)
-    plt.xticks(range(-1,21),
-               [ 'S','P',1,'', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20], rotation=0
-               ,fontsize=15);
-    plt.yticks(fontsize=15)
-    plt.grid('on')
-    sns.despine()
+
+    if show_plot:
+        plt.figure()
+        plt.plot(range(-1,21),landscape, marker='s');
+        plt.xlabel('targeting progression', fontsize=15)
+        plt.ylabel(r'(approx.) free-energy ($k_BT$)',fontsize=15)
+        plt.xticks(range(-1,21),
+                   [ 'S','P',1,'', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20], rotation=0
+                   ,fontsize=15);
+        plt.yticks(fontsize=15)
+        plt.grid('on')
+        sns.despine()
 
 
 
@@ -49,22 +51,23 @@ def plot_free_energy_landscape(parameters,model_id):
     free_energy =  -1*np.log( np.cumsum( np.exp(-landscape[1:])   )  )
 
     # plotting:
-    plt.figure()
-    plt.plot(range(0,21),free_energy, marker='s');
-    plt.xlabel('targeting progression', fontsize=15)
-    plt.ylabel(r'(approx.) free-energy ($k_BT$)',fontsize=15)
-    plt.xticks(range(0,21),
-               [ 'P',1,'', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20], rotation=0
-               ,fontsize=15);
-    plt.yticks(fontsize=15)
-    plt.grid('on')
-    sns.despine()
+    if show_plot:
+        plt.figure()
+        plt.plot(range(0,21),free_energy, marker='s');
+        plt.xlabel('targeting progression', fontsize=15)
+        plt.ylabel(r'(approx.) free-energy ($k_BT$)',fontsize=15)
+        plt.xticks(range(0,21),
+                   [ 'P',1,'', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20], rotation=0
+                   ,fontsize=15);
+        plt.yticks(fontsize=15)
+        plt.grid('on')
+        sns.despine()
 
     return landscape, free_energy
 
 
 
-def plot_landscape(parameters, model_id):
+def plot_landscape(parameters, model_id, axis=None):
     '''
     Plot the (free-)energy landscape of the on-target
     :param parameters:
@@ -78,21 +81,29 @@ def plot_landscape(parameters, model_id):
     landscape = [0.0]
     for eps in epsilon_C:
         landscape.append(landscape[-1] + eps)
-    plt.plot(range(-1,21),landscape, marker='s');
+
+    if axis is None:
+        plt.plot(range(-1,21),landscape, marker='s');
+        ax = plt.gca()
+    else:
+        ax = axis
+        ax.plot(range(-1,21),landscape, marker='s');
+
 
     # window dressing:
-    plt.xlabel('targeting progression', fontsize=15)
-    plt.ylabel(r'free-energy ($k_BT$)',fontsize=15)
-    plt.xticks(range(-1,21),
-               ['S', 'P',1,'', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20], rotation=0
-               ,fontsize=15);
-    plt.yticks(fontsize=15)
-    plt.grid('on')
-    sns.despine()
+    ax.set_xlabel('targeting progression', fontsize=15)
+    ax.set_ylabel(r'free-energy ($k_BT$)',fontsize=15)
+    ax.set_xticks(range(-1,21))
+    ax.set_xticklabels( ['S', 'P',1,'', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20],
+                        rotation=0,
+                        fontsize=15);
+    ax.set_yticklabels(ax.get_yticks() ,fontsize=15)
+    ax.grid('on')
+    sns.despine(ax=ax)
     return landscape
 
 
-def plot_mismatch_penalties(parameters, model_id):
+def plot_mismatch_penalties(parameters, model_id,axis=None):
     '''
     plot mismatch penalties VS position as a bar plot
     :param parameters:
@@ -101,13 +112,18 @@ def plot_mismatch_penalties(parameters, model_id):
     '''
     epsilon, fwrd_rates = read_model_ID.unpack_parameters(parameters, model_id, guide_length=20)
     epsilon_I = epsilon[21:]
-    plt.bar(range(1,21), epsilon_I)
 
+    if axis is None:
+        ax = plt.gca()
+    else:
+        ax = axis
+    ax.bar(range(1,21), epsilon_I)
     # window dressing:
-    plt.xlabel('targeting progression', fontsize=15)
-    plt.ylabel(r'mismatch penalties ($k_BT$)',fontsize=15)
-    plt.xticks(np.arange(1,21)+0.5,
-               [1, '', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20], rotation=0
-               ,fontsize=15);
-    plt.yticks(fontsize=15)
+    ax.set_xlabel('targeting progression', fontsize=15)
+    ax.set_ylabel(r'mismatch penalties ($k_BT$)',fontsize=15)
+    ax.set_xticks(np.arange(1,21)+0.5)
+    ax.set_xticklabels([1, '', '', '', 5, '', '', '', '', 10, '', '', '', '', 15, '', '', '', '', 20],
+                       rotation=0,
+                       fontsize=15);
+    ax.set_yticklabels(ax.get_yticks(),fontsize=15)
     return epsilon_I
