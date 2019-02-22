c1b2f64167d913e8d2a32e5090ff33a70050802f
svandersmagt <svandersmagt@gmail.com>
2019-02-22 14:18:02 +0100

    Added a model with predifined epsilon PAM and forward rate from PAM for saturated case

--- code_general/read_model_ID.py
+++ code_general/read_model_ID.py
@@ -26,9 +26,23 @@ def unpack_parameters(parameters, model_id='general_energies',guide_length=20):
     epsilon = np.zeros(2 * guide_length + 1)
     forward_rates = np.ones(guide_length + 2)
 
-    #if model_id == 'Clv_init_limit_Saturated_general_energies_v2':
-    #epsilon_PAM = -100.0
-    #rate_sol_to_PAM = 1000.0
+    if model_id == 'Clv_init_limit_Saturated_general_energies_v2':
+        if len(parameters)!=43:
+            return
+        
+        epsilon[0] = -100.0 #predefined epsilon PAM at saturation
+        epsilon[1:] = parameters[:-3]
+        
+        rate_sol_to_PAM = 10000.0 #predefined at saturation
+        rate_PAM_to_R1 = 10**parameters[-3]
+        rate_internal = 10**parameters[-2]
+        rate_clv = 10**parameters[-1]
+        
+        forward_rates = forward_rates * rate_internal #internal rates
+        forward_rates[0] = rate_sol_to_PAM
+        forward_rates[1] = rate_PAM_to_R1
+        forward_rates[-1] = rate_clv
+                
 
     if model_id == 'Clv_init_limit_general_energies_v2':
         # General position dependency
@@ -42,7 +56,7 @@ def unpack_parameters(parameters, model_id='general_energies',guide_length=20):
         forward_rates = np.ones(guide_length + 2) * rate_internal #internal rates
         forward_rates[0] = rate_sol_to_PAM
         forward_rates[1] = rate_PAM_to_R1
-        forward_rates[-1] = rate_clv  # dCas9 does not cleave
+        forward_rates[-1] = rate_clv
 
     
     if model_id == 'general_energies_no_kPR':
