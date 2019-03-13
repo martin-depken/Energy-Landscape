7e397c315be96840aa1ef1f131b0df32b8f73f79
sfdejong <sonny.dejong@epfl.ch>
2019-03-13 15:04:19 +0100

    Addition of the class Cas

--- Sonny/Pipeline-v01/kinetic_model.py
+++ Sonny/Pipeline-v01/kinetic_model.py
@@ -1,11 +1,9 @@
 import numpy as np
 
-def get_backward_rates(energies, forwardrates, guide_length=20):
+def get_backward_rates(energies, forwardrates, Cas):
 	# kb(n) = kf(n-1) * exp(+epsilon(n))
-    backwardrates = np.zeros(guide_length)
+    backwardrates = np.zeros(Cas.guidelength)
     backwardrates = forwardrates[:-1] * np.exp(energies)
-    #backwardrates = forwardrates[:-1] * np.exp(energies) # version from lab
-    #backwardrates[:-1] = forwardrates[:-1] * np.exp(energies[:-1]) # XXX CHECK THIS!
     return backwardrates
 	
 def build_rate_matrix(forward_rates, backward_rates):
@@ -20,19 +18,3 @@ def mean_first_passage_time(M):
 	onevec = np.ones(len(M))
 	everything_unbound = np.array([1.0] + [0.0] * (guidelength-1))
 	return onevec.dot(np.linalg.solve(-M,everything_unbound))
-	
-def get_backward_rates_2(energies, forward_rates,guide_length=20):
-    '''
-    Apply detailed balance condition to get the backward rates from the energies and forward rates
-
-    :param energies:
-    :param forward_rates:
-    :param guide_length:
-    :return:
-    '''
-    # 0) Construct array containing backward rates
-    backward_rates = np.zeros(guide_length+1)
-
-    # 1) Apply detailed balance condition:
-    backward_rates = forward_rates[:-1] * np.exp(energies)
-    return backward_rates
