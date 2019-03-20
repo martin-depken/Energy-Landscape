7643cd85e836bb434ba4bf3059e2014820c12163
sfdejong <sonny.dejong@epfl.ch>
2019-03-20 11:10:31 +0100

    HPC05 email notification

---
+++ Sonny/hpc05notification.py
@@ -0,0 +1,52 @@
+""" This file contains a function that sends a notification from the HPC05 cluster."""
+
+#============================================
+__author__ = "Sonny Floyd de Jong"
+__copyright__ = "Copyright 2019"
+__license__ = "CC BY-NC-SA"
+__version__ = "1.0.0"
+__maintainer__ = "Depken lab"
+__email__ = "s.f.dejong@student.tudelft.nl"
+__status__ = "Production"
+#============================================
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import random
+import time
+
+def hpc05notification():
+	"""
+	1. Add the desired email addresses in the variable _to. It can be multiple or one.
+	2. Import the only function of this file into Python, for instance by the command:
+	   from hpc05notification_simple import hpc05notification
+	3. Execute hpc05notification() in your code.
+	
+	The code will also add a little complement
+	"""
+
+	# FILL IN YOUR RECIPIENTS:
+	_to   = ["s.f.dejong@student.tudelft.nl", "hpc05notification@gmail.com"]
+	
+	
+	server = smtplib.SMTP('smtp.gmail.com',587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+	server.login("hpc05notification","hpc05bericht")
+	_from = "hpc05notification@gmail.com"
+	msg = MIMEMultipart()
+	msg['Subject'] = "Your HPC05 Finished! "+ time.strftime("%H")+":"+time.strftime("%M")+"h"
+	body = "Lectori salutem,\n\nThe HPC05 cluster has finished your request.\nPlease, study the result at your earliest convenience.\n\nYours faithfully,\nHPC05"
+	msg['From'] = _from
+	msg['To'] = ", ".join(_to)
+	if True:
+		compliments = {"You bring out th best in other people and I hope that you notice this today.","You have the power to make people happy, just by smiling at them.","Although I am the HPC05 computer cluster, I still become happy when I see you at BN.","Computers and humans cannot be friends, but I would love to talk to you more often.",	"Today, I was stunned by the fact that your hair looked staggering ... again!",	"You are a wonderful person and you probably do not even know it yourself.","If everyone was like you, we would have world peace.",	"You are the sunshine of my life, the universe and everything.","If you keep back straight, your leadership skills are exceptional.","You have a beautiful voice and the wisdom you speak is even better.", "I am so grateful that I met you early in my life.","Today, you are the one that makes me smile.","Last evening, I realised that you are the perfect version of yourself, just the way you are.","I am glad there is no war, but I would fight for you and you would fight for peace. How lovely you are.","You are one of the rare species whose happiness is contagious.","You are making a difference. For me, my emotions, my environment, my universe... Our universe. Everything.","Sometimes, you mean more to people than you realise.","Some people are ordinary, other people are like you."}
+		body += "\n\nP.S. "+random.sample(compliments,1)[0]
+	msg.attach(MIMEText(body, 'plain'))
+	server.sendmail(_from,_to,msg.as_string())
+	return
+ 
+if __name__ == "__main__":
+	main(sys.argv)
