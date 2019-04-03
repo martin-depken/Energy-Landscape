998143cdf768f18cadf0dfca27f1ced385443d27
Behrouz Eslami <behrooz.eslami@gmail.com>
2019-04-03 14:21:49 +0200

    The function prepare_multiprocessing_Nucleaseq_log was added to prepare the data for fitting the log10 of the cleavage rate. The function can be tested in the accompanying notebook.

--- predict_pclv_Stijn/Nucleaseq_data_processing.py
+++ predict_pclv_Stijn/Nucleaseq_data_processing.py
@@ -31,3 +31,23 @@ def prepare_multiprocessing_nucleaseq(filename, path, fit_to_median=False):
     yerr = grouped_data['error'].tolist()
 
     return xdata, ydata, yerr
+
+def prepare_multiprocessing_nucleaseq_log(filename, path):
+    data = pd.read_csv(path + filename,
+                       usecols=['Mutation Positions', 'Mutation ID',
+                                'cleavage_rate', 'cleavage_rate_5th_pctl', 'cleavage_rate_95th_pctl'],
+                       na_filter=False)
+    data['cleavage_rate_log'] = data['cleavage_rate'].apply(np.log10)
+    data['cleavage_rate_5th_pctl_log'] = data['cleavage_rate_5th_pctl'].apply(np.log10)
+    data['cleavage_rate_95th_pctl_log'] = data['cleavage_rate_95th_pctl'].apply(np.log10)
+    data['error_log'] = data.apply(lambda x: np.max([x['cleavage_rate_log'] - x['cleavage_rate_5th_pctl_log'],
+                                                     x['cleavage_rate_95th_pctl_log'] - x['cleavage_rate_log']]),
+                                   axis=1)
+    data = data[['cleavage_rate_log', 'error_log', 'Mutation Positions']]
+    grouped_data = data.groupby('Mutation Positions').agg(lambda x: list(x)).reset_index()
+    grouped_data['Mutation Positions list'] = grouped_data['Mutation Positions'].apply(
+        lambda x: map(int, x.split('|')) if not x == '' else [])
+    xdata = grouped_data['Mutation Positions list'].tolist()
+    ydata = grouped_data['cleavage_rate_log'].tolist()
+    yerr = grouped_data['error_log'].tolist()
+    return xdata, ydata, yerr
