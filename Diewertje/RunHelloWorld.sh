7fe4e102979898c151ec9e3a81100f11b6e84d36
Diewertje11 <diewertje@kpnplanet.nl>
2019-04-25 16:27:35 +0200

    the new fits with only 2 and 3 concentration points, and the adjusted code for running these fits.

--- Diewertje/RunHelloWorld.sh
+++ Diewertje/RunHelloWorld.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #PBS -l nodes=3:ppn=3
-
 numbr=$((PBS_NUM_PPN*PBS_NUM_NODES))
-
 module load mpi/openmpi-1.8.8-gnu
 /home/dddekker/miniconda3/envs/dev/bin/mpirun -np $numbr -hostfile $PBS_NODEFILE python /home/dddekker/BEP/Hello_World_MPI.py
 
--- Diewertje/RunPipelineFinkelstein.sh
+++ Diewertje/RunPipelineFinkelstein.sh
@@ -2,7 +2,7 @@
 #PBS -l nodes=1:ppn=20
 #PBS -m e
 #PBS -M diewertje@kpnplanet.nl
-parameters=$(sed -n -e "${PBS_ARRAYID}p" /home/dddekker/BEP/8_4_2019_conc2/jobs_8_4_2019_conc2_1_10.txt)
+parameters=$(sed -n -e "${PBS_ARRAYID}p" /home/dddekker/BEP/8_4_2019_conc3/jobs_8_4_2019_conc3_1_10.txt)
 parameterArray=($parameters) 
 
 ModelID=${parameterArray[0]}
